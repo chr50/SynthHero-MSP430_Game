2dba94b163ac4ea0f27620d67b168ee6c89b9c52
chr50 <116159307+chr50@users.noreply.github.com>
2025-08-25 15:55:11 +0200

    Update i2c.h
    consistency

--- libs/i2c.h
+++ libs/i2c.h
@@ -43,19 +43,33 @@ unsigned char * prxData;    // pointer to the receiver data stack
  * FUNCTION PROTOTYPES
  *****************************************************************************/
 
-// Initialize the I2C state machine. The speed should be 100 kBit/s.
-// <addr> is the 7-bit address of the slave (MSB shall always be 0, i.e. "right alignment"). (2 pts.)
+/**
+ * Init Function to set a slave address and do configurations.
+ * The transmission speed is 100kbit/s, assuming a 16MHZ SMCLK.
+ */
 void i2c_init (unsigned char addr);
 
-// Write a sequence of <length> characters from the pointer <txData>.
-// Return 0 if the sequence was acknowledged, 1 if not. Also stop transmitting further bytes upon a missing acknowledge.
-// Only send a stop condition if <stop> is not 0. (2 pts.)
+/**
+ * Function to write I2C signal from *txData, can be of various length.
+ * Only sends stop if requested (value any other than 0).
+ * Returns 0 if transfer successful, 1 else.
+ */
 unsigned char i2c_write(unsigned char length, unsigned char * txData, unsigned char stop);
 
-// Returns the next <length> characters from the I2C interface. (2 pts.)
+/**
+ * Function to read from the I2C bus and save it in rxData.
+ * Returns 0 if transfer uccessful, else 1.
+ */
 void i2c_read(unsigned char length, unsigned char * rxData);
 
+/**
+ * Implementation of the TX ISR in I2C
+ */
 void i2c_tx_isr(void);
+
+/**
+ * Implementation of the RX ISR in I2C
+ */
 void i2c_rx_isr(void);
 
 #endif /* EXERCISE_LIBS_I2C_H_ */
